67d3c71d61f7620c146df080a0f78bade65babb8
Kalaivani Ravi <kalaivaniravi2208@gmail.com>
2025-07-16 12:49:09 +0000

    added Treatment  tag

--- models/transformation/t_treatments.sql
+++ models/transformation/t_treatments.sql
@@ -3,7 +3,8 @@
     config(
         schema = 'transformation',
         pre_hook=["{{start_log( invocation_id) }}"],
-        post_hook=["{{ end_log( invocation_id) }}"]
+        post_hook=["{{ end_log( invocation_id) }}"],
+        tags=['treatments']
     )
 }}
 
