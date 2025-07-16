8764a1af7fa9816087dd74b5781122a7bb8395e5
Kalaivani Ravi <kalaivaniravi2208@gmail.com>
2025-07-16 11:14:29 +0000

    added tags to treatments flow

--- models/intermediate/int_treatment_scd.sql
+++ models/intermediate/int_treatment_scd.sql
@@ -4,7 +4,8 @@
         materialized='incremental',
         unique_key = 'treatment_key',
         pre_hook=["{{start_log( invocation_id) }}"],
-        post_hook=["{{ end_log( invocation_id) }}"]
+        post_hook=["{{ end_log( invocation_id) }}"],
+        tags = 'treatments'
     )
 }}
 
